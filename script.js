14d537c3db4b7bbf95ec19a41747e4bc41f250bd
shomi2013 <roxley2013@gmail.com>
2024-09-10 10:25:27 -0300

    commit inicial

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const productos = [
     {nombre: 'Tobillera', precio: 5000},
 ]
 
+productos.push ('Aros con Dije', precio: 5000)
+console.log (productos)
+
 const carrito = []
 
 const agregarCarrito = (productos, cantidad)=>{
@@ -29,10 +32,10 @@ const totalCarrito = ()=>{
 }
 
 const app = ()=>{
-    alert("Bienvenidos a Tienda Mushu")
+    alert("Bienvenidos a Joyeria Mushu")
     let loop = true
     while(loop){
-        let producto = prompt("Por favor elegir el Código del producto que desea comprar: \n 0- Anillo de Plata $ 5000 \n 1- Cadena $10000 \n 2- Pulsera $8000 \n 3- Dije de Corazón $6000 \n 4- Media Falange $4000 \n 5- Tobillera $5000")
+        let producto = prompt("Por favor elegir el Código del producto que desea comprar: \n 0- Anillo de Plata $ 5000 \n 1- Cadena $10000 \n 2- Pulsera $8000 \n 3- Dije de Corazón $6000 \n 4- Media Falange $4000 \n 5- Tobillera $5000 \n 6- Aros con Dije $5000")
         let cantidad = parseInt(prompt("Por favor, indicar la cantidad de ${producto} que quiere comprar"))
         agregarCarrito(productos[producto], cantidad)
         loop = confirm("¿Desea agregar mas productos a su compra?")
